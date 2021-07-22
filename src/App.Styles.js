eda462f5f2a83dd1b6bcde7b98d1438c9bafa310
Joãõ Paulo Moretti <joaopaulomrtt@gmail.com>
2021-07-21 23:03:17 -0300

    fiz o filter estatico

---
+++ src/App.Styles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components"
+
+export const Filtro = styled.div`
+  border: 1px solid black;
+  height: 100vh;
+  width: 20%;
+  color: white;
+  background-color: #1B1B1B;
+`
