a5a242d1f97f2a2b60d54efffb4fa65f1fccf339
Ivan Gomes <ivanjunior.pt@gmail.com>
2021-07-23 16:38:51 -0300

    Versão atualizada do container de produtos

--- src/components/ProductContainer.js
+++ src/components/ProductContainer.js
@@ -1,6 +1,26 @@
 import React from 'react'
-
 import styled from 'styled-components';
+import Products from './Products'
+
+
+const SortStyle = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border: 1px solid black;
+  width: 60vw;
+  height: 10vh;
+
+  p {
+      padding: 7px;
+  }
+
+  select {
+      margin: 10px;
+      width: 6vw;
+      height: 4vh;
+  }
+`
 
 
 const StyledContainer = styled.div`
@@ -43,43 +63,70 @@ const ProductCard = styled.div`
     }
 `
 
+
+
 class ProductContainer extends React.Component {
+    state = {
+        sort: 'Descrecente'
+    }
+
+    onChangeSort = (event) => {
+        this.setState({
+            sort: event.target.value
+        })
+    }
+
+
     render() {
+        const products = [
+            {
+                id: 1,
+                name: 'Camisa do Astrodev',
+                price: 90,
+                image: 'https://ae01.alicdn.com/kf/H3d8970ee9584491babdea172dd3f5e00U/2020-casual-3d-t-camisa-das-mulheres-dos-homens-crian-as-espa-o-astronauta-planeta-explorar.jpg_q50.jpg'
+            },
+            {
+                id: 2,
+                name: 'Camisa Planeta Dark',
+                price: 110,
+                image: 'https://cf.shopee.com.br/file/ab9827659f02f23d6efc9241a4dd8611'
+            },
+            {
+                id: 3,
+                name: 'Camisa Galáxia',
+                price: 70,
+                image: 'https://http2.mlstatic.com/D_NQ_NP_865720-MLB31699567510_082019-O.jpg'
+            },
+            {
+                id: 4,
+                name: 'Camisa da Nasa',
+                price: 90,
+                image: 'https://static.netshoes.com.br/produtos/camiseta-geek-cool-tees-nasa-vintage/14/HIK-0027-014/HIK-0027-014_zoom1.jpg?ts=1626104468&ims=544x'
+            }
+
+        ]
+
         return (
-            <StyledContainer>
-                <ProductCard>
-                    <img src="https://ae01.alicdn.com/kf/H3d8970ee9584491babdea172dd3f5e00U/2020-casual-3d-t-camisa-das-mulheres-dos-homens-crian-as-espa-o-astronauta-planeta-explorar.jpg_q50.jpg" alt="Imagem de Camisa" />
+            <div>
+                <SortStyle>
+                    <p>Quantidade de produtos: 4</p>
+                    <select value={this.state.sort} Onchange={this.onChangeSort}>
+                        <option>Crescente</option>
+                        <option>Descrecente</option>
+                    </select>
+                </SortStyle>
 
-                    <p>Camisa do Astrodev</p>
-                    <p>R$90,00</p>
-                    <button>Adicionar ao carrinho</button>
-                </ProductCard>
+                <StyledContainer>
 
-                <ProductCard>
-                    <img src="https://cf.shopee.com.br/file/ab9827659f02f23d6efc9241a4dd8611" alt="Imagem de Camisa" />
-
-                    <p>Camisa Planeta Dark</p>
-                    <p>R$110,00</p>
-                    <button>Adicionar ao carrinho</button>
-                </ProductCard>
-
-                <ProductCard>
-                    <img src="https://http2.mlstatic.com/D_NQ_NP_865720-MLB31699567510_082019-O.jpg" alt="Imagem de Camisa" />
-
-                    <p>Camisa Galáxia</p>
-                    <p>R$70,00</p>
-                    <button>Adicionar ao carrinho</button>
-                </ProductCard>
-
-
-                <ProductCard>
-                    <img src="https://static.netshoes.com.br/produtos/camiseta-geek-cool-tees-nasa-vintage/14/HIK-0027-014/HIK-0027-014_zoom1.jpg?ts=1626104468&ims=544x" alt="Imagem de camisa" />
-
-                    <p>Camisa da Nasa</p>
-                    <p>R$120,00</p>
-                    <button>Adicionar ao carrinho</button>
-                </ProductCard>
-            </StyledContainer>
+                    {products.map((product, index) => {
+                        return <ProductCard key={index}>
+                            <Products
+                                products={product}
+                            />
+                        </ProductCard>
+                    })}
+                </StyledContainer>
+            </div>
         )
     }
 }
